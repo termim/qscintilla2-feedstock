f5c277a89961ea769d2462b76cff91822251b31f
Daniel Schreij <dschreij@gmail.com>
2017-01-28 16:29:40 -0400

    Support for Qt5

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,18 +3,6 @@
 :: installed in the user's AppData folder. Add it to PATH
 setlocal EnableDelayedExpansion
 
-if "%PY_VER%"=="2.7" (
-	set VC9DIR=%LOCALAPPDATA%\Programs\Common\Microsoft\Visual C++ for Python\9.0
-	IF EXIST !VC9DIR! ( 
-		set PATH=!VC9DIR!;"%PATH%"
-		if %ARCH% == 32 call vcvarsall.bat x86
-		if %ARCH% == 64 call vcvarsall.bat amd64
-		:: vcvarsall does something strange with PATH, preventing xcopy to be found later. Fix this
-		set PATH=!PATH!;%SYSTEMROOT%\system32
-	)
-)
-if errorlevel 1 exit 1
-
 :: Determine Qt Major verion (4 or 5)
 for /f "tokens=4" %%i in ('%LIBRARY_BIN%\qmake -v ^| find "Qt version"') do (
 	set QT_MAJOR_VER=%%i
